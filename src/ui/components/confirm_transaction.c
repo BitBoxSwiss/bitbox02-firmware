a5533440a715e24f3b0fa123af42d0ac76455978
Marko Bencun <marko@shiftcrypto.ch>
2019-08-05 20:20:28 +0200

    components/confirm_transaction: use confirm_button

--- src/ui/components/confirm_transaction.c
+++ src/ui/components/confirm_transaction.c
@@ -21,7 +21,8 @@
 
 typedef struct {
     bool has_address;
-    void (*confirm_callback)(component_t*);
+    void (*cancel_callback)(void);
+    void (*confirm_callback)(void);
 } data_t;
 
 static void _render(component_t* component)
@@ -38,9 +39,18 @@ static void _on_event(const event_t* event, component_t* component)
     if (event->id == EVENT_CONFIRM) {
         data_t* data = (data_t*)component->data;
         if (data->confirm_callback) {
-            data->confirm_callback(NULL);
+            data->confirm_callback();
             data->confirm_callback = NULL;
         }
+    }
+}
+
+static void _cancel(component_t* cancel_button)
+{
+    component_t* component = cancel_button->parent;
+    data_t* data = (data_t*)component->data;
+    if (data->cancel_callback != NULL) {
+        data->cancel_callback();
     }
 }
 
@@ -62,8 +72,8 @@ static component_t* _confirm_transaction_create(
     const char* address,
     const char* fee,
     bool verify_total, /* if true, verify total and fee, otherwise verify amount and address */
-    void (*confirm_callback)(component_t*),
-    void (*cancel_callback)(component_t*))
+    void (*confirm_callback)(void),
+    void (*cancel_callback)(void))
 {
     if (address && fee) {
         Abort("Error: confirm btc does not support displaying both address and fee");
@@ -84,19 +94,16 @@ static component_t* _confirm_transaction_create(
 
     data->has_address = strlens(address);
     data->confirm_callback = confirm_callback;
+    data->cancel_callback = cancel_callback;
     confirm->data = data;
     confirm->f = &_component_functions;
     confirm->dimension.width = SCREEN_WIDTH;
     confirm->dimension.height = SCREEN_HEIGHT;
 
-    ui_util_add_sub_component(
-        confirm, icon_button_create(top_slider, ICON_BUTTON_CROSS, cancel_callback));
-    if (verify_total) {
-        ui_util_add_sub_component(confirm, confirm_gesture_create(confirm));
-    } else {
-        ui_util_add_sub_component(
-            confirm, icon_button_create(top_slider, ICON_BUTTON_CHECK, confirm_callback));
-    }
+    ui_util_add_sub_component(confirm, icon_button_create(top_slider, ICON_BUTTON_CROSS, _cancel));
+
+    bool longtouch = verify_total;
+    ui_util_add_sub_component(confirm, confirm_button_create(longtouch));
 
     if (data->has_address) {
         char addr[128];
@@ -126,8 +133,8 @@ static component_t* _confirm_transaction_create(
 component_t* confirm_transaction_address_create(
     const char* amount,
     const char* address,
-    void (*confirm_callback)(component_t*),
-    void (*cancel_callback)(component_t*))
+    void (*confirm_callback)(void),
+    void (*cancel_callback)(void))
 {
     return _confirm_transaction_create(
         amount, address, NULL, false, confirm_callback, cancel_callback);
@@ -136,8 +143,8 @@ component_t* confirm_transaction_address_create(
 component_t* confirm_transaction_fee_create(
     const char* amount,
     const char* fee,
-    void (*confirm_callback)(component_t*),
-    void (*cancel_callback)(component_t*))
+    void (*confirm_callback)(void),
+    void (*cancel_callback)(void))
 {
     return _confirm_transaction_create(amount, NULL, fee, true, confirm_callback, cancel_callback);
 }

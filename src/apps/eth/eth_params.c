d3cec48bf0bd512518e8b2348d5d519e162113d0
Niklas Claesson <nicke.claesson@gmail.com>
2020-01-15 21:23:56 +0100

    eth: organize tokens

--- src/apps/eth/eth_params.c
+++ src/apps/eth/eth_params.c
@@ -62,30 +62,23 @@ static const app_eth_erc20_params_t _erc20_params[] = {
     /* Ethereum */
     {
         .coin = ETHCoin_ETH,
-        .unit = "USDT",
+        .unit = "BAT",
         .contract_address =
-            "\xda\xc1\x7f\x95\x8d\x2e\xe5\x23\xa2\x20\x62\x06\x99\x45\x97\xc1\x3d\x83\x1e\xc7",
-        .decimals = 6,
+            "\x0d\x87\x75\xf6\x48\x43\x06\x79\xa7\x09\xe9\x8d\x2b\x0c\xb6\x25\x0d\x28\x87\xef",
+        .decimals = 18,
     },
     {
         .coin = ETHCoin_ETH,
-        .unit = "USDC",
+        .unit = "DAI",
         .contract_address =
-            "\xa0\xb8\x69\x91\xc6\x21\x8b\x36\xc1\xd1\x9d\x4a\x2e\x9e\xb0\xce\x36\x06\xeb\x48",
-        .decimals = 6,
+            "\x6b\x17\x54\x74\xe8\x90\x94\xc4\x4d\xa9\x8b\x95\x4e\xed\xea\xc4\x95\x27\x1d\x0f",
+        .decimals = 18,
     },
     {
         .coin = ETHCoin_ETH,
         .unit = "LINK",
         .contract_address =
             "\x51\x49\x10\x77\x1a\xf9\xca\x65\x6a\xf8\x40\xdf\xf8\x3e\x82\x64\xec\xf9\x86\xca",
-        .decimals = 18,
-    },
-    {
-        .coin = ETHCoin_ETH,
-        .unit = "BAT",
-        .contract_address =
-            "\x0d\x87\x75\xf6\x48\x43\x06\x79\xa7\x09\xe9\x8d\x2b\x0c\xb6\x25\x0d\x28\x87\xef",
         .decimals = 18,
     },
     {
@@ -97,13 +90,6 @@ static const app_eth_erc20_params_t _erc20_params[] = {
     },
     {
         .coin = ETHCoin_ETH,
-        .unit = "ZRX",
-        .contract_address =
-            "\xe4\x1d\x24\x89\x57\x1d\x32\x21\x89\x24\x6d\xaf\xa5\xeb\xde\x1f\x46\x99\xf4\x98",
-        .decimals = 18,
-    },
-    {
-        .coin = ETHCoin_ETH,
         .unit = "SAI",
         .contract_address =
             "\x89\xd2\x4a\x6b\x4c\xcb\x1b\x6f\xaa\x26\x25\xfe\x56\x2b\xdd\x9a\x23\x26\x03\x59",
@@ -111,9 +97,23 @@ static const app_eth_erc20_params_t _erc20_params[] = {
     },
     {
         .coin = ETHCoin_ETH,
-        .unit = "DAI",
+        .unit = "USDC",
         .contract_address =
-            "\x6b\x17\x54\x74\xe8\x90\x94\xc4\x4d\xa9\x8b\x95\x4e\xed\xea\xc4\x95\x27\x1d\x0f",
+            "\xa0\xb8\x69\x91\xc6\x21\x8b\x36\xc1\xd1\x9d\x4a\x2e\x9e\xb0\xce\x36\x06\xeb\x48",
+        .decimals = 6,
+    },
+    {
+        .coin = ETHCoin_ETH,
+        .unit = "USDT",
+        .contract_address =
+            "\xda\xc1\x7f\x95\x8d\x2e\xe5\x23\xa2\x20\x62\x06\x99\x45\x97\xc1\x3d\x83\x1e\xc7",
+        .decimals = 6,
+    },
+    {
+        .coin = ETHCoin_ETH,
+        .unit = "ZRX",
+        .contract_address =
+            "\xe4\x1d\x24\x89\x57\x1d\x32\x21\x89\x24\x6d\xaf\xa5\xeb\xde\x1f\x46\x99\xf4\x98",
         .decimals = 18,
     },
 };

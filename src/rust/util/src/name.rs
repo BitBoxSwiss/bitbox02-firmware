f21cc4f2ad52f067905b32125084583f92e2e661
Marko Bencun <marko@shiftcrypto.ch>
2020-03-16 14:32:57 +0100

    rust: add license headers

--- src/rust/util/src/name.rs
+++ src/rust/util/src/name.rs
@@ -1,3 +1,17 @@
+// Copyright 2020 Shift Cryptosecurity AG
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /// Validate a user given name. The name must be smaller or equal to `max_len` and larger than 0 in
 /// size, consist of printable ASCII characters only (and space), not
 /// start or end with whitespace, and contain no whitespace other than space.

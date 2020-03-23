BitBoxSwiss/bitbox02-firmware

commit aba96f99579214620eb92fa5a7e2abd7d223548d

unlock: Move get_mnemonic_passphrase workflow to a separate file.

src/workflow/get_mnemonic_passphrase.c
@@ -1,0 +1,40 @@
+#include "get_mnemonic_passphrase.h"
+
+#include <string.h>
+
+#include <hardfault.h>
+#include <ui/components/trinary_input_string.h>
+#include <ui/fonts/password_11X12.h>
+
+#include "blocking.h"
+#include "confirm.h"
+#include "password_enter.h"
+#include "status.h"
+
+bool get_mnemonic_passphrase(char* passphrase_out)
+{
+    if (passphrase_out == NULL) {
+        Abort("_get_mnemonic_passphrase");
+    }
+    while (true) {
+        password_enter_blocking("Enter\noptional passphrase", true, passphrase_out);
+        if (strlen(passphrase_out) == 0) {
+            // No need to confirm the empty passphrase.
+            break;
+        }
+        const confirm_params_t params = {
+            .title = "",
+            .body = "You will be asked to\nvisually confirm your\npassphrase now.",
+            .accept_only = true,
+        };
+        if (!workflow_confirm_blocking(&params)) {
+            return false;
+        }
+        if (workflow_confirm_scrollable_longtouch_blocking(
+                "Confirm", passphrase_out, &font_password_11X12)) {
+            break;
+        }
+        workflow_status_blocking("Please try again", false);
+    }
+    return true;
+}

src/CMakeLists.txt
@@ -44,6 +44,7 @@ set(DBB-FIRMWARE-SOURCES
   ${CMAKE_SOURCE_DIR}/src/workflow/confirm.c
   ${CMAKE_SOURCE_DIR}/src/workflow/confirm_time.c
   ${CMAKE_SOURCE_DIR}/src/workflow/create_seed.c
+  ${CMAKE_SOURCE_DIR}/src/workflow/get_mnemonic_passphrase.c
   ${CMAKE_SOURCE_DIR}/src/workflow/idle_workflow.c
   ${CMAKE_SOURCE_DIR}/src/workflow/pairing.c
   ${CMAKE_SOURCE_DIR}/src/workflow/password.c

src/workflow/get_mnemonic_passphrase.h
@@ -1,0 +1,7 @@
+#ifndef _GET_MNEMONIC_PASSPHRASE_H
+#define _GET_MNEMONIC_PASSPHRASE_H
+
+#include <stdbool.h>
+bool get_mnemonic_passphrase(char* passphrase_out);
+
+#endif // _GET_MNEMONIC_PASSPHRASE_H

src/workflow/unlock.c
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #include "unlock.h"
-#include "confirm.h"
 #include "password_enter.h"
 #include "status.h"
 #include "workflow.h"
@@ -23,43 +22,15 @@
 #include <screen.h>
 #include <string.h>
 #include <ui/components/ui_images.h>
-#include <ui/fonts/password_11X12.h>
 #include <ui/screen_stack.h>
 #include <ui/ugui/ugui.h>
 #include <util.h>
+#include <workflow/get_mnemonic_passphrase.h>
 #ifndef TESTING
 #include <hal_delay.h>
 #endif
 
 #include <stdio.h>
-
-static bool _get_mnemonic_passphrase(char* passphrase_out)
-{
-    if (passphrase_out == NULL) {
-        Abort("_get_mnemonic_passphrase");
-    }
-    while (true) {
-        password_enter_blocking("Enter\noptional passphrase", true, passphrase_out);
-        if (strlen(passphrase_out) == 0) {
-            // No need to confirm the empty passphrase.
-            break;
-        }
-        const confirm_params_t params = {
-            .title = "",
-            .body = "You will be asked to\nvisually confirm your\npassphrase now.",
-            .accept_only = true,
-        };
-        if (!workflow_confirm_blocking(&params)) {
-            return false;
-        }
-        if (workflow_confirm_scrollable_longtouch_blocking(
-                "Confirm", passphrase_out, &font_password_11X12)) {
-            break;
-        }
-        workflow_status_blocking("Please try again", false);
-    }
-    return true;
-}
 
 bool workflow_unlock_bip39(void)
 {
@@ -67,7 +38,7 @@ bool workflow_unlock_bip39(void)
     char mnemonic_passphrase[SET_PASSWORD_MAX_PASSWORD_LENGTH] = {0};
     UTIL_CLEANUP_STR(mnemonic_passphrase);
     if (memory_is_mnemonic_passphrase_enabled()) {
-        if (!_get_mnemonic_passphrase(mnemonic_passphrase)) {
+        if (!get_mnemonic_passphrase(mnemonic_passphrase)) {
             return false;
         }
     }

commit d34b5a88ab35e64f4e28507d7a7fde709ba0b682

workflow/get_mnemonic_passphrase: make visual confirmation scrollable
regression from aba96f99

src/workflow/get_mnemonic_passphrase.c
@@ -105,6 +105,7 @@ static void _get_mnemonic_passphrase_spin(workflow_t* self)
         const confirm_params_t passphrase_params = {.title = "Confirm",
                                                     .body = data->passphrase,
                                                     .font = &font_password_11X12,
+                                                    .scrollable = true,
                                                     .longtouch = true};
         workflow_stack_start_workflow(
             workflow_confirm(&passphrase_params, _confirm_passphrase_cb, data));
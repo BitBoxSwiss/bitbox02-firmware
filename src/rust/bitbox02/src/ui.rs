288c88ea99fd46b1a96dd2103cd9d4ecc4aa634b
Marko Bencun <marko@shiftcrypto.ch>
2020-03-25 23:46:41 +0100

    bitbox02-rust: add async confirm workflow

--- src/rust/bitbox02/src/ui.rs
+++ src/rust/bitbox02/src/ui.rs
@@ -54,6 +54,89 @@ pub fn trinary_input_string_create_password(
     Component(component)
 }
 
+pub enum Font {
+    Default,
+    Password11X12,
+}
+
+impl Font {
+    fn as_ptr(&self) -> *const bitbox02_sys::UG_FONT {
+        match self {
+            Font::Default => core::ptr::null() as *const _,
+            Font::Password11X12 => unsafe { &bitbox02_sys::font_password_11X12 },
+        }
+    }
+}
+
+impl Default for Font {
+    fn default() -> Self {
+        return Font::Default;
+    }
+}
+
+#[derive(Default)]
+pub struct ConfirmParams<'a> {
+    /// The confirmation title of the screen. Max 200 chars, otherwise **panic**.
+    pub title: &'a str,
+    /// The confirmation body of the screen. Max 200 chars, otherwise **panic**.
+    pub body: &'a str,
+    pub font: Font,
+    /// If true, the body is horizontally scrollable.
+    pub scrollable: bool,
+    /// If true, require the hold gesture to confirm instead of tap.
+    pub longtouch: bool,
+    /// If true, the user can only confirm, not reject.
+    pub accept_only: bool,
+    /// if true, the accept icon is a right arrow instead of a checkmark (indicating going to the
+    /// "next" screen).
+    pub accept_is_nextarrow: bool,
+    /// if true, will only show first and last 32 bytes.
+    pub shorten_body: bool,
+    /// Print the value of this variable in the corner. Will not print when 0
+    pub display_size: usize,
+}
+
+/// Creates a user confirmation dialog screen.
+/// `result` - will be asynchronously set to `Some(bool)` once the user accets or rejects.
+pub fn confirm_create(params: &ConfirmParams, result: Pin<&mut Option<bool>>) -> Component {
+    let params = bitbox02_sys::confirm_params_t {
+        title: crate::str_to_cstr_force!(params.title, 200).as_ptr(),
+        body: crate::str_to_cstr_force!(params.body, 200).as_ptr(),
+        font: params.font.as_ptr(),
+        scrollable: params.scrollable,
+        longtouch: params.longtouch,
+        accept_only: params.accept_only,
+        accept_is_nextarrow: params.accept_is_nextarrow,
+        shorten_body: params.shorten_body,
+        display_size: params.display_size as _,
+    };
+
+    extern "C" fn on_accept_cb(param: *mut c_void) {
+        let mut out: Box<Pin<&mut Option<bool>>> = unsafe { Box::from_raw(param as *mut _) };
+        out.set(Some(true));
+    }
+    extern "C" fn on_reject_cb(param: *mut c_void) {
+        let mut out: Box<Pin<&mut Option<bool>>> = unsafe { Box::from_raw(param as *mut _) };
+        out.set(Some(false));
+    }
+
+    let result_ptr = Box::into_raw(Box::new(result)) as *mut _; // passed to callbacks `param`.
+    let component = unsafe {
+        bitbox02_sys::confirm_create(
+            &params,
+            Some(on_accept_cb),
+            result_ptr,
+            if !params.accept_only {
+                Some(on_reject_cb)
+            } else {
+                None
+            },
+            result_ptr,
+        )
+    };
+    Component(component)
+}
+
 pub fn screen_stack_push(component: &mut Component) {
     unsafe {
         bitbox02_sys::ui_screen_stack_push(component.0);

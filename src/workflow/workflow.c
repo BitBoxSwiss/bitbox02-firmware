0318148bc6178b31407d9d783da85116415654f9
Marko Bencun <marko@shiftcrypto.ch>
2020-08-05 15:16:17 +0200

    workflow: remove all C workflow things
    Now all done via Rust.

--- src/workflow/workflow.c
+++ src/workflow/workflow.c
@@ -25,7 +25,6 @@
 #include <ui/components/confirm.h>
 #include <ui/components/waiting.h>
 #include <ui/screen_stack.h>
-#include <ui/workflow_stack.h>
 #include <util.h>
 
 static void _confirm_dismiss(bool result, void* param)
@@ -43,40 +42,4 @@ void workflow_confirm_dismiss(const char* title, const char* body)
         .accept_only = true,
     };
     ui_screen_stack_switch(confirm_create(&params, _confirm_dismiss, NULL));
-}
-
-workflow_t* workflow_allocate(
-    workflow_method init,
-    workflow_method cleanup,
-    workflow_method spin,
-    size_t data_size)
-{
-    workflow_t* result = (workflow_t*)malloc(sizeof(*result));
-    if (!result) {
-        Abort("malloc failed in workflow_allocate");
-    }
-    result->init = init;
-    result->cleanup = cleanup;
-    result->spin = spin;
-    result->data_size = data_size;
-    if (!data_size) {
-        result->data = NULL;
-    } else {
-        result->data = calloc(1, data_size);
-        if (!result->data) {
-            Abort("Workflow data malloc failed.");
-        }
-    }
-    return result;
-}
-
-void workflow_destroy(workflow_t* workflow)
-{
-    if (workflow->cleanup) {
-        workflow->cleanup(workflow);
-    }
-    util_zero(workflow->data, workflow->data_size);
-    free(workflow->data);
-    util_zero(workflow, sizeof(*workflow));
-    free(workflow);
 }

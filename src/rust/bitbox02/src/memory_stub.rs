de12c5da0577dc5f819050096a67fa1869e33230
Marko Bencun <marko@shiftcrypto.ch>
2020-08-31 15:39:29 +0200

    rust/bitbox02: add stubs for testing
    The bitbox02 safely wraps C functions. In Rust tests, C is not linked,
    and even if it was, properly mocking the functions would be difficult.
    
    We do a workaround instead: by adding the 'testing' feature and
    enabling it only in tests, we can supply stub/mock implementations
    that can be used in unit tests, not referencing any C functions.
    
    In future commits, we can add some light mocking infrastructure to be
    able to check args and mock the return values.
    
    Alternatives dismissed:
    - Add a trait for memory, keystore, ui and mock those.
      This resulted in all the code littered with generic params and lots
      of boilerplate.
    - Implement the C funtions in Rust using cdylib and use that:
      Too complicated compared to the testing feature.

---
+++ src/rust/bitbox02/src/memory_stub.rs
@@ -0,0 +1,51 @@
+// Copyright 2020 Shift Crypto AG
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Stubs for testing.
+
+pub fn set_device_name(_name: &str) -> Result<(), ()> {
+    panic!("not implemented")
+}
+
+pub fn is_initialized() -> bool {
+    panic!("not implemented")
+}
+
+pub fn is_mnemonic_passphrase_enabled() -> bool {
+    panic!("not implemented")
+}
+
+pub fn get_attestation_pubkey_and_certificate(
+    _device_pubkey: &mut [u8; 64],
+    _certificate: &mut [u8; 64],
+    _root_pubkey_identifier: &mut [u8; 32],
+) -> Result<(), ()> {
+    panic!("not implemented")
+}
+
+pub fn bootloader_hash(_out: &mut [u8; 32]) {
+    panic!("not implemented")
+}
+
+pub fn get_noise_static_private_key() -> Result<zeroize::Zeroizing<[u8; 32]>, ()> {
+    panic!("not implemented")
+}
+
+pub fn check_noise_remote_static_pubkey(_pubkey: &[u8; 32]) -> bool {
+    panic!("not implemented")
+}
+
+pub fn add_noise_remote_static_pubkey(_pubkey: &[u8; 32]) -> Result<(), ()> {
+    panic!("not implemented")
+}

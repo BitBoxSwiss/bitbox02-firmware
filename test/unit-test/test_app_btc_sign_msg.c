47432c4dc34248e4fe58e2f44c40ed8e523ceb53
Marko Bencun <marko@shiftcrypto.ch>
2020-10-05 19:31:44 +0200

    apps/btc: add unit test for app_btc_sign_msg

---
+++ test/unit-test/test_app_btc_sign_msg.c
@@ -0,0 +1,109 @@
+// Copyright 2020 Shift Crypto AG
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <cmocka.h>
+
+#include <stdio.h>
+
+#include <apps/btc/btc_sign_msg.h>
+#include <keystore.h>
+#include <rust/rust.h>
+#include <workflow/confirm.h>
+
+bool __wrap_workflow_confirm_blocking(const confirm_params_t* params)
+{
+    check_expected(params->body);
+    return true;
+}
+
+VerifyMessageResult __wrap_rust_workflow_verify_message(Bytes msg)
+{
+    return VerifyMessageResultOk;
+}
+
+static uint8_t _mock_seed[32] = {
+    0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22,
+    0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44,
+};
+
+static uint8_t _mock_bip39_seed[64] = {
+    0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22,
+    0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44,
+    0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22,
+    0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44,
+};
+
+static void _test_btc_sign_msg(void** state)
+{
+    const BTCScriptConfig script_config = {
+        .which_config = BTCScriptConfig_simple_type_tag,
+        .config =
+            {
+                .simple_type = BTCScriptConfig_SimpleType_P2WPKH_P2SH,
+            },
+    };
+    uint32_t keypath[] = {
+        49 + BIP32_INITIAL_HARDENED_CHILD,
+        0 + BIP32_INITIAL_HARDENED_CHILD,
+        0 + BIP32_INITIAL_HARDENED_CHILD,
+        0,
+        0,
+    };
+    const uint8_t msg[] = "sign this message";
+    uint8_t signature[65] = {0};
+
+    mock_state(_mock_seed, _mock_bip39_seed);
+    expect_string(__wrap_workflow_confirm_blocking, params->body, "Coin: Bitcoin");
+    expect_string(
+        __wrap_workflow_confirm_blocking, params->body, "32af55mrChF2FUjZo8rSuCbxFy5MEqjCeo");
+    assert_int_equal(
+        APP_BTC_OK,
+        app_btc_sign_msg(
+            BTCCoin_BTC,
+            &script_config,
+            keypath,
+            sizeof(keypath) / sizeof(uint32_t),
+            msg,
+            sizeof(msg) - 1,
+            signature));
+    const uint8_t expected_signature[65] =
+        "\x19\x1b\xcd\x29\x28\x6f\x95\x6a\xf5\x3b\x87\x88\x88\x4c\x91\x1e\xcb\x29\x69\xdb\xcb\x1d"
+        "\xa5\x88\xd1\x57\xce\x9b\x6a\x6b\x60\x6b\x3d\xc9\x0a\x12\x74\x7a\x9d\xef\x4d\x7c\xaa\x72"
+        "\x8b\xcf\x0c\xb3\x0d\xd2\xdc\x93\xba\xdd\xed\x9e\x55\xbd\x1c\x5e\x80\xe9\x78\x8a\x01";
+    assert_memory_equal(signature, expected_signature, sizeof(expected_signature));
+
+    // Keypath does not match script config.
+    keypath[0] = 84 + BIP32_INITIAL_HARDENED_CHILD;
+    assert_int_equal(
+        APP_BTC_ERR_INVALID_INPUT,
+        app_btc_sign_msg(
+            BTCCoin_BTC,
+            &script_config,
+            keypath,
+            sizeof(keypath) / sizeof(uint32_t),
+            msg,
+            sizeof(msg) - 1,
+            signature));
+}
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(_test_btc_sign_msg),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}

b9e3e4343f9a539ad2e236ba5f44df05b8fc5764
Marko Bencun <marko@shiftcrypto.ch>
2023-01-03 00:05:10 +0100

    cardano/xpubs: unit test xpubs api call

--- src/rust/bitbox02-rust/src/hww/api/cardano/xpubs.rs
+++ src/rust/bitbox02-rust/src/hww/api/cardano/xpubs.rs
@@ -37,3 +37,87 @@ pub fn process(request: &pb::CardanoXpubsRequest) -> Result<Response, Error> {
     }
     Ok(Response::Xpubs(pb::CardanoXpubsResponse { xpubs }))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use bitbox02::testing::mock_unlocked;
+    use util::bip32::HARDENED;
+
+    #[test]
+    fn test_process() {
+        bitbox02::keystore::lock();
+        assert_eq!(
+            process(&pb::CardanoXpubsRequest { keypaths: vec![] }),
+            Ok(Response::Xpubs(pb::CardanoXpubsResponse { xpubs: vec![] })),
+        );
+
+        // Locked.
+        assert_eq!(
+            process(&pb::CardanoXpubsRequest {
+                keypaths: vec![pb::Keypath {
+                    keypath: vec![1852 + HARDENED, 1815 + HARDENED, HARDENED]
+                }],
+            }),
+            Err(Error::Generic),
+        );
+
+        mock_unlocked();
+        assert_eq!(
+            process(&pb::CardanoXpubsRequest {
+                keypaths: vec![
+                    pb::Keypath {
+                        keypath: vec![1852 + HARDENED, 1815 + HARDENED, HARDENED]
+                    },
+                    pb::Keypath {
+                        keypath: vec![1852 + HARDENED, 1815 + HARDENED, 1 + HARDENED]
+                    }
+                ],
+            }),
+            Ok(Response::Xpubs(pb::CardanoXpubsResponse {
+                xpubs: vec![
+                    vec![
+                        135, 93, 21, 165, 177, 234, 235, 114, 75, 217, 61, 109, 54, 203, 75, 97,
+                        188, 69, 219, 186, 120, 219, 156, 176, 139, 147, 231, 40, 146, 89, 211,
+                        216, 174, 223, 100, 1, 197, 31, 45, 152, 27, 1, 127, 215, 4, 53, 226, 217,
+                        223, 160, 215, 78, 124, 206, 75, 146, 6, 29, 251, 8, 139, 95, 8, 206
+                    ],
+                    vec![
+                        205, 217, 152, 187, 63, 149, 35, 26, 115, 72, 234, 223, 192, 248, 151, 77,
+                        20, 221, 211, 158, 71, 189, 60, 40, 26, 217, 150, 150, 122, 49, 129, 126,
+                        93, 199, 240, 91, 226, 212, 218, 106, 29, 25, 36, 178, 129, 146, 0, 184,
+                        113, 4, 22, 225, 46, 250, 1, 192, 77, 21, 220, 167, 234, 215, 191, 233
+                    ]
+                ],
+            })),
+        );
+
+        // Invalid keypaths
+        let invalid_keypaths: &[&[u32]] = &[
+            // Invalid purpose
+            &[1851 + HARDENED, 1815 + HARDENED, HARDENED],
+            // Invalid coin
+            &[1852 + HARDENED, 1814 + HARDENED, HARDENED],
+            // Account too low
+            &[1852 + HARDENED, 1815 + HARDENED, 0],
+            &[1852 + HARDENED, 1815 + HARDENED, HARDENED - 1],
+            // Account too high
+            &[1852 + HARDENED, 1815 + HARDENED, 100 + HARDENED],
+            // Wrong number of elements (too short)
+            &[1852 + HARDENED, 1815 + HARDENED],
+            // Wrong number of elements (too long)
+            &[1852 + HARDENED, 1815 + HARDENED, HARDENED, 0],
+        ];
+        for invalid_keypath in invalid_keypaths {
+            assert_eq!(
+                process(&pb::CardanoXpubsRequest {
+                    keypaths: vec![pb::Keypath {
+                        keypath: invalid_keypath.to_vec(),
+                    },],
+                }),
+                Err(Error::InvalidInput),
+            );
+        }
+    }
+}

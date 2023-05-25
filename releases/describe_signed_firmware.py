a4e27940da0a228d759be7b16d48317c23f28ea7
Inusual8 <anonymous@admin.com>
2023-05-26 00:26:34 +0200

    Add Assertion and script check sign edit proposal

--- releases/describe_signed_firmware.py
+++ releases/describe_signed_firmware.py
@@ -45,38 +45,47 @@ def main() -> int:
         return 1
 
     with open(filename, "rb") as fileobj:
-        signed_firmware = fileobj.read()
+        file_content = fileobj.read()
 
-    # Split signed firmware into sigdata and firmware
-    if len(signed_firmware) < SIGDATA_LEN:
-        print("firmware too small")
-        return 1
-    magic, rest = signed_firmware[:MAGIC_LEN], signed_firmware[MAGIC_LEN:]
-    sigdata, firmware = rest[:SIGDATA_LEN], rest[SIGDATA_LEN:]
+    firmware_is_signed = len(file_content) >= MAGIC_LEN and file_content[:MAGIC_LEN] in [MAGIC_MULTI, MAGIC_BTCONLY]
 
-    print(
-        "The following information assumes the provided binary was signed correctly; "
-        "the signatures are not being verified."
-    )
-    if magic == MAGIC_MULTI:
-        print("This is a Multi-edition firmware.")
-    elif magic == MAGIC_BTCONLY:
-        print("This is a Bitcoin-only edition firmware.")
+    if firmware_is_signed:
+        # If firmware is signed
+        # Split signed firmware into sigdata and firmware
+        if len(file_content) < SIGDATA_LEN:
+            print("firmware too small")
+            return 1
+        magic, rest = file_content[:MAGIC_LEN], file_content[MAGIC_LEN:]
+        sigdata, firmware = rest[:SIGDATA_LEN], rest[SIGDATA_LEN:]
+
+        print(
+            "The following information assumes the provided binary was signed correctly; "
+            "the signatures are not being verified."
+        )
+        if magic == MAGIC_MULTI:
+            print("This is a Multi-edition firmware.")
+        elif magic == MAGIC_BTCONLY:
+            print("This is a Bitcoin-only edition firmware.")
+        else:
+            print("Unrecognized firmware edition; magic =", magic.hex())
     else:
-        print("Unrecognized firmware edition; magic =", magic.hex())
+        print("This firmware is not signed.")
+        firmware = file_content
 
     firmware_padded = firmware + b"\xFF" * (MAX_FIRMWARE_SIZE - len(firmware))
 
     print(
-        "The hash of the unsigned firmware binary is (compare with reproducible build):"
+        "The hash of the firmware binary is (compare with reproducible build):"
     )
     print(hashlib.sha256(firmware).hexdigest())
-    version = sigdata[SIGNING_PUBKEYS_DATA_LEN:][:VERSION_LEN]
-    print("The monotonic firmware version is:", struct.unpack("<I", version)[0])
-    print("The hash of the firmware as verified/shown by the bootloader is:")
-    print(
-        hashlib.sha256(hashlib.sha256(version + firmware_padded).digest()).hexdigest()
-    )
+    
+    if firmware_is_signed and len(file_content) >= SIGNING_PUBKEYS_DATA_LEN + VERSION_LEN:
+        version = sigdata[SIGNING_PUBKEYS_DATA_LEN:][:VERSION_LEN]
+        print("The monotonic firmware version is:", struct.unpack("<I", version)[0])
+        print("The hash of the firmware as verified/shown by the bootloader is:")
+        print(
+            hashlib.sha256(hashlib.sha256(version + firmware_padded).digest()).hexdigest()
+        )
 
     return 0
 

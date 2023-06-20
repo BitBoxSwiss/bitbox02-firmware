09748481d38b604ae5826288fd88f31c888b8a81
Marko Bencun <marko@shiftcrypto.ch>
2023-06-20 21:43:03 +0200

    miniscript: check for duplicate keys in policy
    In a pubkey reference like `@0/<10;11>/*`, the receive address
    derivation is `<xpub>/10/<addressIndex>` and the change deriation is
    `<xpub>/11/<addressIndex>`. The pubkey reference would be replaced by
    a pubkey derived at a particular change/receive derivation.
    
    We check that a policy cannot contain the same pubkeys under any
    derivation because Miniscript prohibits duplicate keys.

--- src/rust/bitbox02-rust/src/hww/api/bitcoin/policies.rs
+++ src/rust/bitbox02-rust/src/hww/api/bitcoin/policies.rs
@@ -23,6 +23,8 @@ use alloc::vec::Vec;
 
 use core::str::FromStr;
 
+use util::bip32::HARDENED;
+
 // Arbitrary limit of keys that can be present in a policy.
 const MAX_KEYS: usize = 20;
 
@@ -53,6 +55,43 @@ fn is_our_key(key: &pb::KeyOriginInfo) -> Result<bool, ()> {
     }
 }
 
+/// Parses a wallet policy of the form `@NUM1/<NUM2;NUM3>/*` into `(NUM1,NUM2,NUM3)`.
+/// `@NUM/**` is equivalent o `@NUM/<0;1>/*`.
+/// The derivation numbers are checked to be distinct and unhardened.
+fn parse_wallet_policy_pk(pk: &str) -> Result<(usize, u32, u32), ()> {
+    fn validate_no_leading_zero(num: &str) -> Result<(), ()> {
+        if num.len() > 1 && num.starts_with('0') {
+            Err(())
+        } else {
+            Ok(())
+        }
+    }
+    let (left, right) = pk.strip_prefix('@').ok_or(())?.split_once('/').ok_or(())?;
+    validate_no_leading_zero(left)?;
+    let (receive_index, change_index): (u32, u32) = match right {
+        "**" => (0, 1),
+        right => {
+            let (left_number_str, right_number_str) = right
+                .strip_prefix('<')
+                .ok_or(())?
+                .strip_suffix(">/*")
+                .ok_or(())?
+                .split_once(';')
+                .ok_or(())?;
+            validate_no_leading_zero(left_number_str)?;
+            validate_no_leading_zero(right_number_str)?;
+            (
+                left_number_str.parse().or(Err(()))?,
+                right_number_str.parse().or(Err(()))?,
+            )
+        }
+    };
+    if receive_index == change_index || receive_index >= HARDENED || change_index >= HARDENED {
+        return Err(());
+    }
+    Ok((left.parse().or(Err(()))?, receive_index, change_index))
+}
+
 /// See `ParsedPolicy`.
 #[derive(Debug)]
 pub struct Wsh<'a> {
@@ -75,12 +114,57 @@ impl<'a> ParsedPolicy<'a> {
         }
     }
 
+    /// Check that it is impossible to create a derivation with duplicate pubkeys, assuming all the
+    /// keys in the key vector are distinct.
+    ///
+    /// Even though the rust-miniscript library checks for duplicate keys, it does so on the raw
+    /// miniscript, which would not catch e.g. that `wsh(or_b(pk(@0/<0;1>/*),s:pk(@0/<2;1>/*)))` has
+    /// a duplicate change derivation if we derive at the receive path.
+    ///
+    /// Also checks that each key is used, e.g. if there are 3 keys in the key vector, @0, @1 and @2
+    /// must be present.
+    fn validate_keys(&self) -> Result<(), Error> {
+        match self {
+            Self::Wsh(Wsh {
+                policy,
+                miniscript_expr,
+            }) => {
+                // in "@key_index/<left;right>", keeps track of (key_index,left) and
+                // (key_index,right) to check for duplicates.
+                let mut derivations_seen: Vec<(usize, u32)> = Vec::new();
+
+                let mut keys_seen: Vec<bool> = vec![false; policy.keys.len()];
+
+                for pk in miniscript_expr.iter_pk() {
+                    let (key_index, multipath_index_left, multipath_index_right) =
+                        parse_wallet_policy_pk(&pk).or(Err(Error::InvalidInput))?;
+
+                    if derivations_seen.contains(&(key_index, multipath_index_left)) {
+                        return Err(Error::InvalidInput);
+                    }
+                    derivations_seen.push((key_index, multipath_index_left));
+                    if derivations_seen.contains(&(key_index, multipath_index_right)) {
+                        return Err(Error::InvalidInput);
+                    }
+                    derivations_seen.push((key_index, multipath_index_right));
+
+                    *keys_seen.get_mut(key_index).ok_or(Error::InvalidInput)? = true;
+                }
+
+                if !keys_seen.into_iter().all(|b| b) {
+                    return Err(Error::InvalidInput);
+                }
+                Ok(())
+            }
+        }
+    }
+
     /// Validate a policy.
     /// - Coin is supported (only Bitcoin testnet for now)
     /// - Number of keys
     /// - At least one of the keys is ours
     /// - There are no duplicate or missing xpubs
-    /// - TODO: many more checks.
+    /// - No duplicate keys in the policy
     pub fn validate(&self, coin: BtcCoin) -> Result<(), Error> {
         check_enabled(coin)?;
 
@@ -89,6 +173,8 @@ impl<'a> ParsedPolicy<'a> {
         if policy.keys.len() > MAX_KEYS {
             return Err(Error::InvalidInput);
         }
+
+        self.validate_keys()?;
 
         // Check that at least one key is ours.
         let has_our_key = 'block: {
@@ -118,8 +204,6 @@ impl<'a> ParsedPolicy<'a> {
         if (1..xpubs.len()).any(|i| xpubs[i..].contains(&xpubs[i - 1])) {
             return Err(Error::InvalidInput);
         }
-
-        // TODO: more checks
 
         Ok(())
     }
@@ -155,7 +239,6 @@ mod tests {
 
     use crate::bip32::parse_xpub;
     use bitbox02::testing::mock_unlocked;
-    use util::bip32::HARDENED;
 
     const SOME_XPUB_1: &str = "xpub6FMWuwbCA9KhoRzAMm63ZhLspk5S2DM5sePo8J8mQhcS1xyMbAqnc7Q7UescVEVFCS6qBMQLkEJWQ9Z3aDPgBov5nFUYxsJhwumsxM4npSo";
 
@@ -185,6 +268,36 @@ mod tests {
             policy: policy.into(),
             keys: keys.to_vec(),
         }
+    }
+
+    #[test]
+    fn test_parse_wallet_policy_pk() {
+        assert_eq!(parse_wallet_policy_pk("@0/**"), Ok((0, 0, 1)));
+        assert_eq!(parse_wallet_policy_pk("@1/**"), Ok((1, 0, 1)));
+        assert_eq!(parse_wallet_policy_pk("@100/**"), Ok((100, 0, 1)));
+
+        assert_eq!(parse_wallet_policy_pk("@0/<0;1>/*"), Ok((0, 0, 1)));
+        assert_eq!(parse_wallet_policy_pk("@0/<1;2>/*"), Ok((0, 1, 2)));
+        assert_eq!(parse_wallet_policy_pk("@0/<100;101>/*"), Ok((0, 100, 101)));
+        assert_eq!(
+            parse_wallet_policy_pk("@50/<100;101>/*"),
+            Ok((50, 100, 101))
+        );
+
+        assert!(parse_wallet_policy_pk("@00/**").is_err());
+        assert!(parse_wallet_policy_pk("@01/**").is_err());
+        assert!(parse_wallet_policy_pk("@0").is_err());
+        assert!(parse_wallet_policy_pk("@0/").is_err());
+        assert!(parse_wallet_policy_pk("@0/*").is_err());
+        assert!(parse_wallet_policy_pk("0/**").is_err());
+        assert!(parse_wallet_policy_pk("@-1/**").is_err());
+        assert!(parse_wallet_policy_pk("@0/<0;1>/*/*").is_err());
+        assert!(parse_wallet_policy_pk("@0/<0;1>").is_err());
+        assert!(parse_wallet_policy_pk("@0/<0;1>/").is_err());
+        assert!(parse_wallet_policy_pk("@0/<100;100>/*").is_err());
+        // 2147483648 = HARDENED offset.
+        assert!(parse_wallet_policy_pk("@0/<100;2147483648>/*").is_err());
+        assert!(parse_wallet_policy_pk("@0/<2147483648;100>/*").is_err());
     }
 
     #[test]
@@ -319,5 +432,86 @@ mod tests {
             .validate(BtcCoin::Tbtc),
             Err(Error::InvalidInput)
         );
+
+        // Not all keys are used.
+        assert_eq!(
+            parse(&make_policy(
+                "wsh(pk(@0/**))",
+                &[our_key.clone(), make_key(SOME_XPUB_1)]
+            ))
+            .unwrap()
+            .validate(BtcCoin::Tbtc),
+            Err(Error::InvalidInput)
+        );
+
+        // Referenced key does not exist
+        assert_eq!(
+            parse(&make_policy("wsh(pk(@1/**))", &[our_key.clone()]))
+                .unwrap()
+                .validate(BtcCoin::Tbtc),
+            Err(Error::InvalidInput)
+        );
+    }
+
+    #[test]
+    fn test_parse_check_dups_in_policy() {
+        mock_unlocked();
+
+        let coin = BtcCoin::Tbtc;
+        let our_key = make_our_key(KEYPATH_ACCOUNT);
+
+        // Ok, one key.
+        let pol = make_policy("wsh(pk(@0/**))", &[our_key.clone()]);
+        assert!(parse(&pol).unwrap().validate(coin).is_ok());
+
+        // Ok, two keys.
+        let pol = make_policy(
+            "wsh(or_b(pk(@0/**),s:pk(@1/**)))",
+            &[our_key.clone(), make_key(SOME_XPUB_1)],
+        );
+        assert!(parse(&pol).unwrap().validate(coin).is_ok());
+
+        // Ok, one key with different derivations
+        let pol = make_policy(
+            "wsh(or_b(pk(@0/<0;1>/*),s:pk(@0/<2;3>/*)))",
+            &[our_key.clone()],
+        );
+        assert!(parse(&pol).unwrap().validate(coin).is_ok());
+
+        // Duplicate path, one time in change, one time in receive. While the keys technically are
+        // never duplicate in the final miniscript with the pubkeys inserted, we still prohibit, as
+        // it does not look like there would be a sane use case for this and would likely be an
+        // accident.
+        let pol = make_policy(
+            "wsh(or_b(pk(@0/<0;1>/*),s:pk(@0/<1;2>/*)))",
+            &[our_key.clone()],
+        );
+        assert!(parse(&pol).unwrap().validate(coin).is_err());
+
+        // Duplicate key inside policy.
+        let pol = make_policy("wsh(or_b(pk(@0/**),s:pk(@0/**)))", &[our_key.clone()]);
+        assert!(parse(&pol).is_err());
+
+        // Duplicate key inside policy (same change and receive).
+        let pol = make_policy("wsh(pk(@0/<0;0>/*))", &[our_key.clone()]);
+        assert!(parse(&pol).unwrap().validate(coin).is_err());
+
+        // Duplicate key inside policy, using different notations for the same thing.
+        let pol = make_policy("wsh(or_b(pk(@0/**),s:pk(@0/<0;1>/*)))", &[our_key.clone()]);
+        assert!(parse(&pol).unwrap().validate(coin).is_err());
+
+        // Duplicate key inside policy, using same receive but different change.
+        let pol = make_policy(
+            "wsh(or_b(pk(@0/<0;1>/*),s:pk(@0/<0;2>/*)))",
+            &[our_key.clone()],
+        );
+        assert!(parse(&pol).unwrap().validate(coin).is_err());
+
+        // Duplicate key inside policy, using same change but different receive.
+        let pol = make_policy(
+            "wsh(or_b(pk(@0/<0;1>/*),s:pk(@0/<2;1>/*)))",
+            &[our_key.clone()],
+        );
+        assert!(parse(&pol).unwrap().validate(coin).is_err());
     }
 }

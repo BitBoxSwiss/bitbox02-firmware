3bac5c6daf6680aaae24490c35895dab01f2cd8e
Niklas Dusenlund <niklas@dusenlund.se>
2024-02-05 11:44:11 +0100

    Makefile had some bugs from previous way we compiled semihosting binaries

--- src/platform/platform_init.c
+++ src/platform/platform_init.c
@@ -18,6 +18,7 @@
 #if !defined(BOOTLOADER)
 #include "sd_mmc/sd_mmc_start.h"
 #endif
+#include "util.h"
 
 extern void initialise_monitor_handles(void);
 
@@ -25,6 +26,7 @@ void platform_init(void)
 {
 #if defined(SEMIHOSTING)
     initialise_monitor_handles();
+    traceln("%s", "Semihosting enabled");
 #endif
     oled_init();
 #if !defined(BOOTLOADER)

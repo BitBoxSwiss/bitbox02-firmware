94de92aae5e52ba4084df33f3e3535b5cf999d4c
Marko Bencun <marko@shiftcrypto.ch>
2024-05-14 10:49:31 +0200

    eth/erc20_params: use packed struct
    This reduces the space efficiency.
    
    Before: 756400 bytes
    After: 749440 bytes
    Diff: -6960 bytes

--- src/rust/bitbox02-rust/src/hww/api/ethereum/erc20_params.rs
+++ src/rust/bitbox02-rust/src/hww/api/ethereum/erc20_params.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[repr(packed)]
 struct P {
     pub unit: *const u8,
     pub contract_address: [u8; 20],

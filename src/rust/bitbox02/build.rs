e9620145e2a31fbd8f7da390ca00acdd37811ecc
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2024-08-31 11:42:25 +0200

    rust: Output warning so that it is visible without `-vv`
    Using `-vv` with cargo (cargo build -vv) does output the stderr/stdout
    of build scripts. But printing "cargo::warning" will emit the message
    without the flags.
    
    Since rust 1.77, the syntax `cargo::` is preferred over the old
    `cargo:`.

--- src/rust/bitbox02/build.rs
+++ src/rust/bitbox02/build.rs
@@ -2,27 +2,28 @@ fn main() {
     #[cfg(feature = "testing")]
     {
         if let Ok(cmake_dir) = std::env::var("CMAKE_CURRENT_BINARY_DIR") {
-            println!("cargo:rustc-link-search={}/../lib", cmake_dir);
+            println!("cargo::rustc-link-search={}/../lib", cmake_dir);
             // c and rust code merged :O
-            println!("cargo:rustc-link-lib=bitbox_merged");
+            println!("cargo::rustc-link-lib=bitbox_merged");
             println!(
-                "cargo:rerun-if-changed={}/../lib/libbitbox_merged.a",
+                "cargo::rerun-if-changed={}/../lib/libbitbox_merged.a",
                 cmake_dir
             );
+            println!("cargo::rerun-if-changed=build.rs");
 
             // external libs
-            println!("cargo:rustc-link-lib=wallycore");
-            println!("cargo:rustc-link-lib=secp256k1");
-            println!("cargo:rustc-link-lib=ctaes");
-            println!("cargo:rustc-link-lib=fatfs");
-            println!("cargo:rustc-link-lib=sd-mock");
+            println!("cargo::rustc-link-lib=wallycore");
+            println!("cargo::rustc-link-lib=secp256k1");
+            println!("cargo::rustc-link-lib=ctaes");
+            println!("cargo::rustc-link-lib=fatfs");
+            println!("cargo::rustc-link-lib=sd-mock");
 
             // system libs
-            println!("cargo:rustc-link-lib=cmocka");
+            println!("cargo::rustc-link-lib=cmocka");
         } else {
             // This is useful in case project is built by tool that doesn't need to link the final
             // target, like rust-analyzer and clippy.
-            eprintln!("Missing env variable CMAKE_CURRENT_BINARY_DIR, linking will fail");
+            println!("cargo::warning=Missing env variable CMAKE_CURRENT_BINARY_DIR, linking will fail");
         }
     }
 }

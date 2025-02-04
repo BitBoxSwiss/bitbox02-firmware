4481dbf2c2d5de2204657e0ae64ac9d68dad2e5b
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2025-02-04 12:42:21 +0100

    Debug: Adds RTT support to bootloader
    To support "handover" from bootloader to application, the RTT
    allocations are put in the same places in RAM in both projects. Without
    this the J-Link software will only find one of the allocations and
    therefore not read the other.

--- src/rust/vendor/rtt-target/src/debug.rs
+++ src/rust/vendor/rtt-target/src/debug.rs
@@ -1,18 +1,14 @@
-//! This module contains macros that work exactly like thier equivalents without `debug_*`
+//! This module contains macros that work exactly like their equivalents without `debug_*`
 
 /* From init.rs */
 
-/// The same as [`rtt_init`] macro but works only in debug
-///
-/// [`rtt_init`](crate::rtt_init)
+/// The same as [`rtt_init`](crate::rtt_init) macro but works only in debug
 #[macro_export]
 macro_rules! debug_rtt_init {
     ($($arg:tt)*) => (if cfg!(debug_assertions) { $crate::rtt_init!($($arg)*); })
 }
 
-/// The same as [`rtt_init_default`] macro but works only in debug
-///
-/// [`rtt_init_default`](crate::rtt_init_default)
+/// The same as [`rtt_init_default`](crate::rtt_init_default) macro but works only in debug
 #[macro_export]
 macro_rules! debug_rtt_init_default {
     ($($arg:tt)*) => (if cfg!(debug_assertions) { $crate::rtt_init_default!($($arg)*); })
@@ -20,25 +16,19 @@ macro_rules! debug_rtt_init_default {
 
 /* From print.rs */
 
-/// The same as [`rtt_init_print`] macro but works only in debug
-///
-/// [`rtt_init_print`](crate::rtt_init_print)
+/// The same as [`rtt_init_print`](crate::rtt_init_print) macro but works only in debug
 #[macro_export]
 macro_rules! debug_rtt_init_print {
     ($($arg:tt)*) => (if cfg!(debug_assertions) { $crate::rtt_init_print!($($arg)*); })
 }
 
-/// The same as [`rprintln`] macro but works only in debug
-///
-/// [`rprintln`](crate::rprintln)
+/// The same as [`rprintln`](crate::rprintln) macro but works only in debug
 #[macro_export]
 macro_rules! debug_rprintln {
     ($($arg:tt)*) => (if cfg!(debug_assertions) { $crate::rprintln!($($arg)*); })
 }
 
-/// The same as [`rprint`] macro but works only in debug
-///
-/// [`rprint`](crate::rprint)
+/// The same as [`rprint`](crate::rprint) macro but works only in debug
 #[macro_export]
 macro_rules! debug_rprint {
     ($($arg:tt)*) => (if cfg!(debug_assertions) { $crate::rprint!($($arg)*); })

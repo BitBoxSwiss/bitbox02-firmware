08471fa6e92f1968b9d9923d1669f80ea477299d
Marko Bencun <marko@shiftcrypto.ch>
2025-05-08 11:54:23 +0200

    api/bluetooth: only cancel workflow when using BLE
    The host can read the response if called via USB, so we don't need to
    cancel in that case.

--- src/rust/bitbox02-rust/src/hww/api/bluetooth.rs
+++ src/rust/bitbox02-rust/src/hww/api/bluetooth.rs
@@ -164,10 +164,12 @@ async fn process_upgrade(
     if response.is_ok() {
         hal.ui().status("Upgrade\nsuccessful", true).await;
         bitbox02::reset_ble();
-        // Since the Bluetooth host will not be there anymore to read this response, this task will
-        // not be cleared by the executor. We do it manually to make space for the next task upon
-        // reconnection.
-        crate::async_usb::cancel();
+        if bitbox02::communication_mode_ble_enabled() {
+            // Since the Bluetooth host will not be there anymore to read this response, this task
+            // will not be cleared by the executor. We do it manually to make space for the next
+            // task upon reconnection.
+            crate::async_usb::cancel();
+        }
     } else {
         hal.ui().status("Upgrade failed", false).await;
     }

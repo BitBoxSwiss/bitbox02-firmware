537e967e4330fafaaf697a80ac471d5dcb4c9b7a
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2025-05-14 20:09:04 +0200

    da14531: invalid firmware
    If there is no valid firmware UART communication is turned off to avoid
    spending a lot of cpu cycles trying to boot the da14531.
    
    The firmware version reading function must also fail "gracefully" so
    that the "device info" endpoint gets some data. If the metadata_version
    is wrong we return all 0s.

--- src/memory/memory_spi.c
+++ src/memory/memory_spi.c
@@ -48,6 +48,8 @@ bool memory_spi_get_active_ble_firmware(
         if (!MEMEQ(fw_hash, metadata.allowed_firmware_hash, 32)) {
             free(*firmware_out);
             *firmware_out = NULL;
+            // TODO: Register this error with some component that can report the error to the end
+            // user.
             return false;
         }
     }
@@ -58,6 +60,8 @@ bool memory_spi_get_active_ble_firmware(
 
 USE_RESULT bool memory_spi_get_active_ble_firmware_version(struct da14531_firmware_version* version)
 {
+    memset(version, 0, sizeof(struct da14531_firmware_version));
+
     memory_ble_metadata_t metadata = {0};
     memory_get_ble_metadata(&metadata);
     util_log("ble active index: %d", metadata.active_index);
@@ -67,19 +71,21 @@ USE_RESULT bool memory_spi_get_active_ble_firmware_version(struct da14531_firmwa
 
     uint32_t ble_addr = metadata.active_index == 0 ? MEMORY_SPI_BLE_FIRMWARE_1_ADDR
                                                    : MEMORY_SPI_BLE_FIRMWARE_2_ADDR;
-    uint8_t* firmware = spi_mem_read(ble_addr + 0x110, sizeof(struct da14531_firmware_version));
-    ASSERT(firmware);
-    if (!firmware) {
+    uint8_t* firmware_bytes =
+        spi_mem_read(ble_addr + 0x110, sizeof(struct da14531_firmware_version));
+    ASSERT(firmware_bytes);
+    if (!firmware_bytes) {
         return false;
     }
 
-    memcpy((uint8_t*)version, firmware, sizeof(struct da14531_firmware_version));
-    free(firmware);
-
-    ASSERT(version->metadata_version == 1);
-
-    if (version->metadata_version == 1) {
-        return true;
+    struct da14531_firmware_version* firmware = (struct da14531_firmware_version*)firmware_bytes;
+    if (firmware->metadata_version == 1) {
+        memcpy((uint8_t*)version, firmware_bytes, sizeof(struct da14531_firmware_version));
+    } else {
+        util_log("Invalid metadata version! No firmware?");
     }
-    return false;
+
+    free(firmware_bytes);
+
+    return true;
 }

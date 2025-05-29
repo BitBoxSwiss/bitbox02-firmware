9e4b45d554e65dc75fc7bd2cbeb010f708a53d24
Marko Bencun <marko@shiftcrypto.ch>
2025-05-29 11:57:30 +0200

    keystore: port keystore_bip85_bip39 to Rust

--- src/rust/bitbox02-sys/build.rs
+++ src/rust/bitbox02-sys/build.rs
@@ -67,7 +67,6 @@ const ALLOWLIST_FNS: &[&str] = &[
     "delay_us",
     "empty_create",
     "keystore_bip39_mnemonic_to_seed",
-    "keystore_bip85_bip39",
     "keystore_copy_seed",
     "keystore_create_and_store_seed",
     "keystore_encode_xpub_at_keypath",

a3f3b28be9c2451e35836cbb8455d8e8ca96470f
benma's agent <codex.upstate405@passmail.net>
2025-12-12 17:31:07 +0100

    license: replace Shift Apache headers
    Replace the existing Shift Crypto/Shift Devices Apache-2.0 boilerplate header blocks with the standard SPDX License Identifier tag: "SPDX-License-Identifier: Apache-2.0".
    
    This is better than the previous multi-line text header because it is machine-readable, unambiguous, and widely understood by tooling (scanners, compliance automation), while keeping files smaller and reducing header churn.

--- tools/atecc608/config/config_test.go
+++ tools/atecc608/config/config_test.go
@@ -1,20 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+
 // Copied and adapted from https://github.com/cesanta/mos-tool/tree/837916dcb506d5cd2adbff02f2cd06ea4ea8c71a/mos/atca
 // https://github.com/cesanta/mos-tool/blob/837916dcb506d5cd2adbff02f2cd06ea4ea8c71a/mos/LICENSE
 // Copyright (c) 2014-2018 Cesanta Software Limited
-// Copyright 2019 Shift Cryptosecurity AG
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package config
 
 import (

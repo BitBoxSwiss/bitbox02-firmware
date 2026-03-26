d5e9e9f1d0ad2ea61589262dc7eb57ae45a060f7
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2026-03-26 13:26:30 +0100

    feat: add LVGL LTDC support

--- src/rust/bitbox-lvgl-sys/wrapper.h
+++ src/rust/bitbox-lvgl-sys/wrapper.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <lvgl.h>
+#include <src/drivers/display/st_ltdc/lv_st_ltdc.h>
 
 extern const lv_font_t inter_regular_32;
 extern const lv_font_t inter_regular_48;

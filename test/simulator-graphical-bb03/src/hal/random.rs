f5fae7e6c8b115c7ebe051ad8f82794ed8cbb898
Marko Bencun <marko@shiftcrypto.ch>
2026-04-01 12:52:28 +0000

    simulator-graphical-bb03: implement Random

--- test/simulator-graphical-bb03/src/hal/random.rs
+++ test/simulator-graphical-bb03/src/hal/random.rs
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
+use alloc::boxed::Box;
 use bitbox_hal as hal;
+use rand::Rng;
 
 pub struct BitBox03Random;
 
 impl hal::random::Random for BitBox03Random {
-    fn random_32_bytes(&mut self) -> alloc::boxed::Box<zeroize::Zeroizing<[u8; 32]>> {
-        todo!()
+    fn random_32_bytes(&mut self) -> Box<zeroize::Zeroizing<[u8; 32]>> {
+        let mut out = Box::new(zeroize::Zeroizing::new([0u8; 32]));
+        self.mcu_32_bytes(out.as_mut());
+        out
     }
 
-    fn mcu_32_bytes(&mut self, _out: &mut [u8; 32]) {
-        todo!()
+    fn mcu_32_bytes(&mut self, out: &mut [u8; 32]) {
+        rand::rng().fill(out);
     }
 }

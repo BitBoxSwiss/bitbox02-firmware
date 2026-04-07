BitBoxSwiss/bitbox02-firmware

commit e393670900a6b5064a003dd16d26ff26c813abd3

payment_request: update swapkit pubkey

src/rust/bitbox02-rust/src/hww/api/payment_request.rs
@@ -35,7 +35,7 @@ const IDENTITIES: &[Identity] = &[
     },
     Identity {
         name: "SWAPKIT",
-        public_key: &hex!("03098cba9cde720171796a5c58cb774b0cd19deb62e9b51df5967aefeba34632ff"),
+        public_key: &hex!("02bf5740a2b794b33d73358d7313e9cb260058f3ac6c886fcc388d9f3f0b48a90d"),
     },
     #[cfg(any(feature = "testing", feature = "c-unit-testing"))]
     Identity {

commit 3593da89737b91ea46b6b478564502afd230ba03

fix unit test
Regression from e39367090.

src/rust/bitbox02-rust/src/hww/api/payment_request.rs
@@ -522,7 +522,7 @@ mod tests {
         assert_eq!(swapkit_identity.name, "SWAPKIT");
         assert_eq!(
             swapkit_identity.public_key,
-            hex!("03098cba9cde720171796a5c58cb774b0cd19deb62e9b51df5967aefeba34632ff")
+            hex!("02bf5740a2b794b33d73358d7313e9cb260058f3ac6c886fcc388d9f3f0b48a90d")
         );
 
         assert_eq!(find_identity("SWAPKIT Provider").unwrap().name, "SWAPKIT");
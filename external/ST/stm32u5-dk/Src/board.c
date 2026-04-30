41f46395c44d2c9fb3cad76c96b22091cd90603c
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2026-04-30 13:34:02 +0200

    add HAL hash

--- external/ST/stm32u5-dk/Src/board.c
+++ external/ST/stm32u5-dk/Src/board.c
@@ -22,6 +22,7 @@
 #include "dsihost.h"
 #include "flash.h"
 #include "gpu2d.h"
+#include "hash.h"
 #include "hspi.h"
 #include "i2c.h"
 #include "icache.h"
@@ -160,6 +161,7 @@ int board_init(void)
   //MX_LTDC_Init();
   //MX_USBX_Device_Init();
   MX_FLASH_Init();
+  MX_HASH_Init();
   /* USER CODE BEGIN 2 */
   // ^- The call to MX_DSIHOST_DSI_Init() must be done before MX_LTDC_Init().
   
@@ -231,7 +233,7 @@ int board_init_essentials(void)
   SystemClock_Config();
   PeriphCommonClock_Config();
 
-  //MX_USART1_UART_Init();
+  MX_HASH_Init();
 
   return 0;
 }

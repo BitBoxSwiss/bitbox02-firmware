660dc0aeb63e5b37e8644d438c777f2aaf5833ca
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2026-06-09 08:26:40 +0200

    bb03 flash: wrap read/write

--- src/rust/bitbox-platform-stm32u5/src/flash.rs
+++ src/rust/bitbox-platform-stm32u5/src/flash.rs
@@ -4,12 +4,17 @@ use core::marker::PhantomData;
 
 use bitbox_platform_stm32u5_sys as ffi;
 
+const FLASH_BANK_SIZE: u32 = ffi::FLASH_SIZE_DEFAULT / 2;
+
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Error {
     Hal(HalError),
     InvalidBootAddress,
+    InvalidAddress,
+    InvalidLength,
+    PageErase(u32),
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -50,6 +55,50 @@ impl UnlockedFlash {
         ensure_hal_ok(unsafe { ffi::HAL_FLASH_OB_Unlock() })?;
         Ok(UnlockedOptionBytes {
             _flash: PhantomData,
+        })
+    }
+
+    pub fn erase_page(&mut self, address: u32) -> Result<()> {
+        if address % ffi::FLASH_PAGE_SIZE != 0 {
+            return Err(Error::InvalidAddress);
+        }
+        let (bank, page) = bank_page(address)?;
+        let mut page_error = u32::MAX;
+        let mut erase = ffi::FLASH_EraseInitTypeDef {
+            TypeErase: ffi::FLASH_TYPEERASE_PAGES,
+            Banks: bank,
+            Page: page,
+            NbPages: 1,
+        };
+        critical_section::with(|_| {
+            ensure_hal_ok(unsafe { ffi::HAL_FLASHEx_Erase(&mut erase, &mut page_error) })
+        })
+        .map_err(|err| match err {
+            Error::Hal(_) if page_error != u32::MAX => Error::PageErase(page_error),
+            err => err,
+        })
+    }
+
+    pub fn program_quadword(&mut self, address: u32, data: &[u8; 16]) -> Result<()> {
+        if address % 16 != 0 {
+            return Err(Error::InvalidAddress);
+        }
+        if !is_valid_flash_range(address, data.len() as u32) {
+            return Err(Error::InvalidAddress);
+        }
+
+        let mut words = [0u32; 4];
+        for (word, bytes) in words.iter_mut().zip(data.chunks_exact(4)) {
+            *word = u32::from_le_bytes(bytes.try_into().unwrap());
+        }
+        critical_section::with(|_| {
+            ensure_hal_ok(unsafe {
+                ffi::HAL_FLASH_Program(
+                    ffi::FLASH_TYPEPROGRAM_QUADWORD,
+                    address,
+                    words.as_ptr() as u32,
+                )
+            })
         })
     }
 }
@@ -107,8 +156,38 @@ pub fn boot_address(config: BootAddressConfig) -> u32 {
     option_bytes.BootAddr
 }
 
+pub fn read(address: u32, out: &mut [u8]) -> Result<()> {
+    if !is_valid_flash_range(address, out.len() as u32) {
+        return Err(Error::InvalidAddress);
+    }
+    unsafe {
+        core::ptr::copy_nonoverlapping(address as *const u8, out.as_mut_ptr(), out.len());
+    }
+    Ok(())
+}
+
 fn is_valid_boot_address(address: u32) -> bool {
     address & !ffi::FLASH_NSBOOTADD0R_NSBOOTADD0 == 0
+}
+
+fn is_valid_flash_range(address: u32, len: u32) -> bool {
+    let Some(end) = address.checked_add(len) else {
+        return false;
+    };
+    address >= ffi::FLASH_BASE_NS && end <= ffi::FLASH_BASE_NS + ffi::FLASH_SIZE_DEFAULT
+}
+
+fn bank_page(address: u32) -> Result<(u32, u32)> {
+    if !is_valid_flash_range(address, ffi::FLASH_PAGE_SIZE) {
+        return Err(Error::InvalidAddress);
+    }
+    let offset = address - ffi::FLASH_BASE_NS;
+    let (bank, bank_offset) = if offset < FLASH_BANK_SIZE {
+        (ffi::FLASH_BANK_1, offset)
+    } else {
+        (ffi::FLASH_BANK_2, offset - FLASH_BANK_SIZE)
+    };
+    Ok((bank, bank_offset / ffi::FLASH_PAGE_SIZE))
 }
 
 fn ensure_hal_ok(status: ffi::HAL_StatusTypeDef) -> Result<()> {

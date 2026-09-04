9b84d92be58f95c1e2104bdffd6f983a6e455298
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2026-09-04 12:48:23 +0200

    fixup! bootloader: use reduced LVGL font assets

--- src/ui/fonts/arial_9_bootloader.h
+++ src/ui/fonts/arial_9_bootloader.h
@@ -1,8 +1,8 @@
-#ifndef _ARIAL_9_BOOTLOADER_H_
-#define _ARIAL_9_BOOTLOADER_H_
+#ifndef TTF2LVGL_ARIAL_9_BOOTLOADER_H
+#define TTF2LVGL_ARIAL_9_BOOTLOADER_H
 
 #include <ugui.h>
 
 extern const UG_FONT font_arial_9;
 
-#endif
+#endif /* TTF2LVGL_ARIAL_9_BOOTLOADER_H */
--- src/ui/fonts/monogram_16_bootloader.h
+++ src/ui/fonts/monogram_16_bootloader.h
@@ -1,8 +1,8 @@
-#ifndef _MONOGRAM_16_BOOTLOADER_H_
-#define _MONOGRAM_16_BOOTLOADER_H_
+#ifndef TTF2LVGL_MONOGRAM_16_BOOTLOADER_H
+#define TTF2LVGL_MONOGRAM_16_BOOTLOADER_H
 
 #include <ugui.h>
 
 extern const UG_FONT font_monogram_16;
 
-#endif
+#endif /* TTF2LVGL_MONOGRAM_16_BOOTLOADER_H */
